367a2a5330c55f144835b3fc5ea6e0b70f8fa456
Bryant <bryly27@sbcglobal.net>
2015-03-30 21:38:03 -0700

    added favicon

--- application/views/main.php
+++ application/views/main.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title>Bryant Yu</title>
+  <link rel="shortcut icon" href="/assets/images/favicon.png" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
